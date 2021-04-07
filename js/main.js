17eeb6ced408d222f6a8c6db9caeb794d2144d46
Max <sss123123111@yandex.ru>
2021-04-07 06:27:11 +0300

    sdffd

--- js/main.js
+++ js/main.js
@@ -1,69 +1,71 @@
-(function(){
+(function () {
 	var d = document,
-	accordionToggles = d.querySelectorAll('.js-accordionTrigger'),
-	setAria,
-	setAccordionAria,
-	switchAccordion,
-  touchSupported = ('ontouchstart' in window),
-  pointerSupported = ('pointerdown' in window);
-  
-  skipClickDelay = function(e){
-    e.preventDefault();
-    e.target.click();
-  }
+		accordionToggles = d.querySelectorAll('.js-accordionTrigger'),
+		setAria,
+		setAccordionAria,
+		switchAccordion,
+		touchSupported = ('ontouchstart' in window),
+		pointerSupported = ('pointerdown' in window);
 
-		setAriaAttr = function(el, ariaType, newProperty){
+	skipClickDelay = function (e) {
+		e.preventDefault();
+		e.target.click();
+	}
+
+	setAriaAttr = function (el, ariaType, newProperty) {
 		el.setAttribute(ariaType, newProperty);
 	};
-	setAccordionAria = function(el1, el2, expanded){
-		switch(expanded) {
-      case "true":
-      	setAriaAttr(el1, 'aria-expanded', 'true');
-      	setAriaAttr(el2, 'aria-hidden', 'false');
-      	break;
-      case "false":
-      	setAriaAttr(el1, 'aria-expanded', 'false');
-      	setAriaAttr(el2, 'aria-hidden', 'true');
-      	break;
-      default:
+	setAccordionAria = function (el1, el2, expanded) {
+		switch (expanded) {
+			case "true":
+				setAriaAttr(el1, 'aria-expanded', 'true');
+				setAriaAttr(el2, 'aria-hidden', 'false');
+				break;
+			case "false":
+				setAriaAttr(el1, 'aria-expanded', 'false');
+				setAriaAttr(el2, 'aria-hidden', 'true');
+				break;
+			default:
 				break;
 		}
 	};
-//function
-switchAccordion = function(e) {
-  console.log("triggered");
-	e.preventDefault();
-	var thisAnswer = e.target.parentNode.nextElementSibling;
-	var thisQuestion = e.target;
-	if(thisAnswer.classList.contains('is-collapsed')) {
-		setAccordionAria(thisQuestion, thisAnswer, 'true');
-	} else {
-		setAccordionAria(thisQuestion, thisAnswer, 'false');
-	}
-  	thisQuestion.classList.toggle('is-collapsed');
-  	thisQuestion.classList.toggle('is-expanded');
+	//function
+	switchAccordion = function (e) {
+		console.log("triggered");
+		e.preventDefault();
+		var thisAnswer = e.target.parentNode.nextElementSibling;
+		var thisQuestion = e.target;
+		if (thisAnswer.classList.contains('is-collapsed')) {
+			setAccordionAria(thisQuestion, thisAnswer, 'true');
+		} else {
+			setAccordionAria(thisQuestion, thisAnswer, 'false');
+		}
+		thisQuestion.classList.toggle('is-collapsed');
+		thisQuestion.classList.toggle('is-expanded');
 		thisAnswer.classList.toggle('is-collapsed');
 		thisAnswer.classList.toggle('is-expanded');
- 	
-  	thisAnswer.classList.toggle('animateIn');
+
+		thisAnswer.classList.toggle('animateIn');
 	};
-	for (var i=0,len=accordionToggles.length; i<len; i++) {
-		if(touchSupported) {
-      accordionToggles[i].addEventListener('touchstart', skipClickDelay, false);
-    }
-    if(pointerSupported){
-      accordionToggles[i].addEventListener('pointerdown', skipClickDelay, false);
-    }
-    accordionToggles[i].addEventListener('click', switchAccordion, false);
-  }
+	for (var i = 0, len = accordionToggles.length; i < len; i++) {
+		if (touchSupported) {
+			accordionToggles[i].addEventListener('touchstart', skipClickDelay, false);
+		}
+		if (pointerSupported) {
+			accordionToggles[i].addEventListener('pointerdown', skipClickDelay, false);
+		}
+		accordionToggles[i].addEventListener('click', switchAccordion, false);
+	}
 })();
 
 let burger = document.querySelector('.burger');
 let menu = document.querySelector('.menu');
+let body = document.querySelector('body');
 
-burger.onclick = function() {
-  menu.classList.toggle('active');
-  burger.classList.toggle('active');
+burger.onclick = function () {
+	menu.classList.toggle('active');
+	burger.classList.toggle('active');
+	body.classList.toggle('lock');
 };
 
 //  tel
@@ -76,15 +78,13 @@ let whenFocusPhone = e => e.target.value = '+7(';
 //чтоб вводились только цифры
 let checkPhoneKey = key => key >= '0' && key <= '9';
 let checkNumPhone = (e) => {
-    if(!checkPhoneKey(e.target.value[e.target.value.length - 1]) || howDigits(e.target.value) > 11)
-        e.target.value = e.target.value.slice(0, -1);
-    if(e.target.value.length == 6)
-        e.target.value += ')-';
-    if(e.target.value.length == 11 || e.target.value.length == 14)
-        e.target.value += '-';
+	if (!checkPhoneKey(e.target.value[e.target.value.length - 1]) || howDigits(e.target.value) > 11)
+		e.target.value = e.target.value.slice(0, -1);
+	if (e.target.value.length == 6)
+		e.target.value += ')-';
+	if (e.target.value.length == 11 || e.target.value.length == 14)
+		e.target.value += '-';
 }
 telInp.addEventListener('focus', whenFocusPhone);
 telInp.addEventListener('input', checkNumPhone);
 telInp.addEventListener('keydown', checkPhoneKey);
-
-
